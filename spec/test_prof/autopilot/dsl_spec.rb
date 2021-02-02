91e6ad27288a754cb79e11ed950405109389cb28
Ruslan Shakirov <toshiroshak@gmail.com>
2021-02-02 22:08:34 +0300

    Add runner and dsl tests

---
+++ spec/test_prof/autopilot/dsl_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require "test_prof/autopilot/runner"
+
+describe TestProf::Autopilot::Runner do
+  subject { described_class.new }
+
+  let(:logging) { TestProf::Autopilot::Logging }
+
+  describe "#run" do
+    let(:executor_class) { TestProf::Autopilot::ProfilingExecutor }
+    let(:executor) { double("executor") }
+    let(:executor_result) { double("executor_result", report: report) }
+    let(:report) { double("report") }
+
+    it "executes profiling" do
+      expect(executor_class).to receive(:new).with(:test_profiler, {}).and_return(executor)
+      expect(executor).to receive(:start).and_return(executor_result)
+
+      subject.run(:test_profiler, {})
+
+      expect(subject.instance_variable_get("@report")).to eq report
+    end
+  end
+
+  describe "#info" do
+    context "when printable object is a report" do
+      let(:printable_object) { double("report", print: "test report") }
+
+      it "prints info" do
+        expect(printable_object).to receive(:print)
+
+        subject.info(printable_object)
+      end
+    end
+
+    context "without printable object" do
+      it "logs message" do
+        expect(logging).to receive(:log).with("Specify data to print: 'report'")
+
+        subject.info
+      end
+    end
+  end
+
+  describe "unknown_instruction" do
+    it "logs message" do
+      expect(logging).to receive(:log).with(
+        <<~MSG
+          'unknown_instruction' instruction is not supported.
+
+          Look to supported instructions: 'run', 'info'.
+        MSG
+      )
+
+      subject.unknown_instruction
+    end
+  end
+end
---
+++ spec/test_prof/autopilot/runner_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "test_prof/autopilot/runner"
+
+describe TestProf::Autopilot::Runner do
+  subject { described_class }
+
+  describe ".invoke" do
+    it "sets up config" do
+      subject.invoke("spec/fixtures/blank_plan.rb", "rspec")
+
+      expect(subject.instance_variable_get("@config")).to be_a TestProf::Autopilot::Runner::Configuration
+      expect(subject.config.command).to eq "rspec"
+      expect(subject.config.plan_path).to eq "spec/fixtures/blank_plan.rb"
+      expect(subject.config.output).to eq $stdout
+    end
+
+    it "creates runner object" do
+      expect(subject).to receive(:new)
+
+      subject.invoke("spec/fixtures/blank_plan.rb", "rspec")
+    end
+
+    it "raises error when without options" do
+      expect { subject.invoke }.to raise_error(ArgumentError)
+    end
+  end
+end
