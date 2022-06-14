26de7897a796dbd55d1d1df979b0e017607eda02
Vladimir Dementyev <dementiev.vm@gmail.com>
2022-06-14 13:40:19 -0400

    feat: add EventProf::Report#merge
    (cherry picked from commit aeb609e54f1c1c1463aa69d5a6e815acb4a1d411)

--- lib/test_prof/autopilot/event_prof/report.rb
+++ lib/test_prof/autopilot/event_prof/report.rb
@@ -25,6 +25,22 @@ module TestProf
         def paths
           @raw_report["groups"].reduce("") { |paths, group| "#{paths} #{group["location"]}" }.strip
         end
+
+        def merge(other)
+          raise ArgumentError, "Incompatible events: #{raw_report["event"]} and #{other.raw_report["event"]}" if raw_report["event"] != other.raw_report["event"]
+
+          report = raw_report.dup
+
+          %w[total_time absolute_run_time total_count].each do |field|
+            report[field] += other.raw_report[field]
+          end
+
+          report["time_percentage"] = (report["total_time"] / report["absolute_run_time"]) * 100
+
+          report["groups"] = (report["groups"] + other.raw_report["groups"]).sort { |a, b| b["time"] <=> a["time"] }.take(report["top_count"])
+
+          Report.new(report)
+        end
       end
     end
   end
--- spec/test_prof/autopilot/event_prof/report_spec.rb
+++ spec/test_prof/autopilot/event_prof/report_spec.rb
@@ -16,10 +16,10 @@ describe TestProf::Autopilot::EventProf::Report do
         {
           "description" => "FirstController",
           "location" => "./spec/controllers/first_controller_spec.rb:1",
-          "time" => 1.289380999514833,
-          "count" => 72,
-          "examples" => 9,
-          "run_time" => 2.059970999951474,
+          "time" => 1.589380999514833,
+          "count" => 123,
+          "examples" => 14,
+          "run_time" => 2.359970999951474,
           "time_percentage" => 62.59
         },
         {
@@ -53,6 +53,68 @@ describe TestProf::Autopilot::EventProf::Report do
       report = subject.build
 
       expect(report.paths).to eq "./spec/controllers/first_controller_spec.rb:1 ./spec/controllers/second_controller_spec.rb:1"
+    end
+  end
+
+  describe "#merge" do
+    let(:aggregated_report) do
+      {
+        "event" => "factory.create",
+        "total_time" => 5.57876199903,
+        "absolute_run_time" => 8.1199419999,
+        "total_count" => 344,
+        "top_count" => 10,
+        "rank_by" => "time",
+        "time_percentage" => 68.70445625225777,
+        "groups" => [
+          {
+            "description" => "FirstModel",
+            "location" => "./spec/models/first_model_spec.rb:1",
+            "time" => 2.0,
+            "count" => 120,
+            "examples" => 10,
+            "run_time" => 2.5,
+            "time_percentage" => 90.0
+          },
+          {
+            "description" => "FirstController",
+            "location" => "./spec/controllers/first_controller_spec.rb:1",
+            "time" => 1.589380999514833,
+            "count" => 123,
+            "examples" => 14,
+            "run_time" => 2.359970999951474,
+            "time_percentage" => 62.59
+          },
+          {
+            "description" => "SecondController",
+            "location" => "./spec/controllers/second_controller_spec.rb:1",
+            "time" => 1.289380999514833,
+            "count" => 72,
+            "examples" => 9,
+            "run_time" => 2.059970999951474,
+            "time_percentage" => 62.59
+          },
+          {
+            "description" => "SecondModel",
+            "location" => "./spec/models/second_model_spec.rb:1",
+            "time" => 1.0,
+            "count" => 80,
+            "examples" => 9,
+            "run_time" => 1.5,
+            "time_percentage" => 66.59
+          }
+        ]
+      }
+    end
+
+    specify do
+      report_1 = described_class.new(JSON.parse(File.read("spec/fixtures/event_prof_report.json")))
+      report_2 = described_class.new(JSON.parse(File.read("spec/fixtures/event_prof_report_2.json")))
+
+      merged = report_1.merge(report_2)
+
+      expect(merged.type).to eq :event_prof
+      expect(merged.raw_report).to eq aggregated_report
     end
   end
 end
